0b28f7ae8399c601f11b50ef4b8ad8565e650b38
HackBomm <hackbomm.net@gmail.com>
2022-08-12 23:24:10 +0600

    yub form validation

--- src/Components/IconBox.js
+++ src/Components/IconBox.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const IconBox = (props) => {
-  const { icon, title, description, styleIcon } = props;
+  const { icon, title, description } = props;
 
   return (
     <div>
       <div className="max-w-sm rounded-lg overflow-hidden shadow-lg my-4 ">
         <div className="flex">
-          {React.createElement(icon, { styleIcon })}
+          {React.createElement(icon)}
           <div className="px-6 py-4">
             <h1 className="font-bold text-xl mb-2">{title}</h1>
             <a href="#s" className="text-blue-700 text-base">
--- src/Components/LeftForm.js
+++ src/Components/LeftForm.js
@@ -1,135 +1,136 @@
 import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 const LeftForm = () => {
-  const [inputData, setInputData] = useState({
-    fullName: "",
-    userName: "",
-    email: "",
-    phoneNumber: "",
-    password: "",
-    confirmPassword: "",
-    url: "",
-    slug: "",
+  const [startDate, setStartDate] = useState(new Date());
+
+  const schema = yup.object({
+    firstName: yup
+      .string()
+      .required("First Name is required")
+      .min(3, "First Name is required"),
+    userName: yup
+      .string()
+      .required("User  Name is required")
+      .min(5, "Minimum charecter is 5"),
   });
 
-  const submitFormHandle = (e) => {
-    e.preventDefault();
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
-  const inputHandle = (e) => {
-    setInputData({
-      ...inputData,
-      [e.target.name]: e.target.value,
-    });
+  const onSubmit = (data) => {
+    console.log(data);
   };
 
   return (
     <>
-      <div className="w-[650px] bg-[#FEFEFE] py-8 px-6 rounded-2xl border mr-4">
+      <div className="w-[650px] bg-[#fcfbfb] py-8 px-6 rounded-2xl border mr-8">
         <h1 className="text-2xl text-black font-bold mb-6 mx-auto">
           Questions? We're Ready To Help You.
         </h1>
 
-        <form className="w-full max-w-lg" onSubmit={submitFormHandle}>
+        <form className="w-full max-w-lg" onSubmit={handleSubmit(onSubmit)}>
           <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
               <input
-                className="shadow-lg appearance-none block w-full  text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white "
-                id="fullName"
+                className="shadow-lg appearance-none block w-full  text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white "
                 name="fullName"
                 type="text"
-                onChange={inputHandle}
                 placeholder="Full Name*"
+                {...register("fullName")}
               />
-              {/* <p className="text-red-500 text-xs italic">
-                Please fill out this field.
-              </p> */}
             </div>
             <div className="w-full md:w-1/2 px-3">
               <input
                 className="shadow-lg appearance-none block w-full  text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-last-name"
                 name="userName"
                 type="text"
                 placeholder="Username*"
+                {...register("userName")}
               />
             </div>
           </div>
           <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
               <input
-                className="shadow-lg appearance-none block w-full  text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                id="email"
+                className="shadow-lg appearance-none block w-full  text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                 name="email"
                 type="email"
-                onChange={inputHandle}
                 placeholder="Email*"
+                {...register("email")}
               />
-              {/* <p className="text-red-500 text-xs italic">
-                Please fill out this field.
-              </p> */}
             </div>
             <div className="w-full md:w-1/2 px-3">
               <input
                 className="shadow-lg appearance-none block w-full  text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-last-name"
                 type="text"
                 name="phoneNumber"
                 placeholder="Phone Number"
+                {...register("phoneNumber")}
               />
             </div>
           </div>
           <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
               <input
-                className="shadow-lg appearance-none block w-full  text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                id="password"
+                className="shadow-lg appearance-none block w-full  text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                 name="password"
                 type="password"
-                onChange={inputHandle}
                 placeholder="Password*"
+                {...register("password")}
               />
-              {/* <p className="text-red-500 text-xs italic">
-                Please fill out this field.
-              </p> */}
             </div>
             <div className="w-full md:w-1/2 px-3">
               <input
                 className="shadow-lg appearance-none block w-full  text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="password"
                 name="confirmPassword"
                 type="password"
                 placeholder="Confirm Password*"
+                {...register("confirmPassword")}
               />
             </div>
           </div>
           <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
               <input
-                className="shadow-lg appearance-none block w-full  text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                id="url"
+                className="shadow-lg appearance-none block w-full  text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                 type="text"
                 name="url"
-                onChange={inputHandle}
                 placeholder="Valid URL"
+                {...register("url")}
               />
-              {/* <p className="text-red-500 text-xs italic">
-                Please fill out this field.
-              </p> */}
             </div>
-            <div className="w-full md:w-1/2 px-3">
-              <input
-                className="shadow-lg appearance-none block w-full  text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="slug"
-                name="slug"
-                type="text"
-                placeholder="Slug"
-              />
+
+            <div className="w-full md:w-1/2 px-3 ">
+              <DatePicker
+                selected={startDate}
+                className="shadow-lg appearance-none block w-full  text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                dateFormat="dd/MM/yyyy"
+                placeholderText='Enter "tomorrow"'
+                fixedHeight
+                dropdownMode="select"
+              ></DatePicker>
             </div>
           </div>
+          <textarea
+            name="question"
+            cols="30"
+            rows="10"
+            className="w-full border-gray-200 ronded focus:bg-white mt-2 mb-6 shadow-2xl"
+            {...register("question")}
+          ></textarea>
           <button
-            className=" w-full shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-            type="button"
+            className="w-full shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+            type="submit"
           >
             Submit
           </button>
@@ -140,3 +141,16 @@ const LeftForm = () => {
 };
 
 export default LeftForm;
+
+// maxDate={new Date()} - maximum date
+// fixedHeight - calender fixed height
+// inline - for fixed design
+// showTimeInput - show calender with time input bottom
+// showTimeSelect - show calender with left time input
+// showMonthYearPicker - Only month picker
+// showFullMonthYearPicker
+// showMonthDropdown - show month as dropdown
+// withPortal - popup
+// todayButton="Hello baby" - footer button
+// showMonthDropdown - Month Dropdown
+// showYearDropdown - year dropdown
